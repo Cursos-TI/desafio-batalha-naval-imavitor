89a11f3a3a1c0f15ffc96a1b3e08f2b3cf88dbf9
Vitor <vitor.imamura@protonmail.com>
2025-12-11 02:24:30 +0000

    Coloquei as Habilidades

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
     int main(){
+        printf("Tabuleiro de Batalha Naval\n");
         printf("  A B C D E F G H I J\n");
         int matriz [10][10] = {0}; //iniciando a matriz 10x10
 
@@ -8,11 +9,11 @@
         int navio3 [3] = {3,3,3};
         int navio4 [3] = {3,3,3};
 
-        //definindo a posição do navio 1, utilizando for para preencher as linhas de acordo com o valor do navio
+        //Definindo a posição do navio 1, utilizando for para preencher as linhas de acordo com o valor do navio
         for (int n1 = 0; n1 < 3; n1++){ 
             matriz[0][n1] = navio1[n1]; //posso decidir em qual linha iniciar (aqui por exemplo começa na linha 1)
         }
-        //definindo a posição do navio 1, utilizando for para preencher as colunas de acordo com o valor do navio
+        //Definindo a posição do navio 1, utilizando for para preencher as colunas de acordo com o valor do navio
         for (int n2 = 0; n2 < 3; n2++){ 
             matriz[n2+3][7] = navio2[n2]; //posso decidir em qual linha iniciar (aqui começa na linha 4)
         }
@@ -24,14 +25,98 @@
             matriz[n4+5][n4+5] = navio4[n4]; 
         }
 
-        for (int i = 0; i < 10; i++){
-            printf("%d ",i+1);  
-            for (int j = 0; j < 10; j++){
+        //Exibição do Tabuleiro Antes do Ataque
+            for (int i = 0; i < 10; i++){
+                printf("%d ",i+1);  
+             for (int j = 0; j < 10; j++){
                 printf("%d ", matriz[i][j]);
             }
           
         printf("\n");
         }
+        printf("\n");
+
+        //Criando as matrizes das habilidades
+        int habilidade1[3][5]={0};
+        int habilidade2[3][5]={0};
+        int habilidade3[3][5]={0};
+        //Cone
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                if( i == 0 && j == 2 ){
+                    habilidade1 [i][j] = 1;
+                }else if( i == 1 &&  j > 0 && j < 4 ){
+                    habilidade1 [i][j] = 1;
+                }else if(i == 2){
+                    habilidade1 [i][j] = 1;
+                    }  
+                }
+            }
+        //Cruz
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                if( i == 0 && j == 2 ){
+                    habilidade2 [i][j] = 1;
+                }else if( i == 1 ){
+                    habilidade2 [i][j] = 1;
+                }else if(i == 2 && j == 2){
+                    habilidade2 [i][j] = 1;
+                    }  
+                }             
+            }
+        //Octaedro
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 5; j++){
+                if( i == 0 && j == 2 ){
+                    habilidade3 [i][j] = 1;
+                }else if( i == 1 &&  j > 0 && j < 4 ){
+                    habilidade3 [i][j] = 1;
+                }else if(i == 2 && j == 2){
+                    habilidade3 [i][j] = 1;
+                    }  
+                }
+            }
+        //Posicionando as habilidades
+        
+        //Cone
+        for (int i = 0; i < 3; i++) //incrementando a linha que começa na quarta linha e vai até a sexta já que a matriz habilidade tem [3][5]
+            for (int j = 0; j < 5; j++){ //mesma coisa para as colunas
+                if (habilidade1[i][j] == 1){
+                    matriz[i][j] = habilidade1[i][j];
+            }
+        } 
+
+        //Cruz
+         for (int i = 5; i < 8; i++){ 
+            for (int j = 2; j < 7; j++){ 
+                if (habilidade2[i-5][j-2] == 1){ 
+                    matriz[i][j] = habilidade2[i-5][j-2];//cálculo para caber nos parametros de busca da matriz original que vai de 0 a 3 e de 0 a 5;
+                }
+            }
+        } 
+
+        //Octaedro
+        for (int i = 0; i < 3; i++){ 
+            for (int j = 5; j < 10; j++){ 
+                if (habilidade3[i][j-5] == 1){ 
+                    matriz[i][j] = habilidade3[i][j-5];
+                }
+            }
+        }
+          
+
+        //Exibição do Tabuleiro Depois do Ataque
+            printf("Resultado do Ataque\n");
+            printf("  A B C D E F G H I J\n");
+            for (int i = 0; i < 10; i++){
+                printf("%d ",i+1);  
+             for (int j = 0; j < 10; j++){
+                printf("%d ", matriz[i][j]);
+            }
+          
+        printf("\n");
+        }
+
     return 0;
 }
 
